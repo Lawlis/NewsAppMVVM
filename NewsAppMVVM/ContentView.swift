d069f3799c019c76bfdb59b8c9b50f1963b8a6ce
Laurynas Letkauskas <letkauskas.laurynas@gmail.com>
2019-11-17 14:12:28 +0200

    Initial Commit

---
+++ NewsAppMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsAppMVVM
+//
+//  Created by Laurynas Letkauskas on 17/11/2019.
+//  Copyright © 2019 Laurynas Letkauskas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
